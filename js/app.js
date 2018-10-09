3b24d10c33e0cf6d136dec9474d5fe1ba6ba5f30
Michael <Jahnsmic001@gmail.com>
2018-10-09 16:54:07 -0700

    Polish to existing features

--- js/app.js
+++ js/app.js
@@ -31,17 +31,17 @@ function Question1(bnum)
 {
     if (bnum == 1)
     {
-        var feedback = "Nice one dumby.";
+        var feedback = "*Nice one dumby.*";
         document.getElementById("questionOneFeedback").innerHTML = feedback
     }
     if (bnum == 2)
     {
-        var feedback = "Almost.";
+        var feedback = "*Almost.*";
         document.getElementById("questionOneFeedback").innerHTML = feedback
     }
     if (bnum == 3)
     {
-        var feedback = "Nioce! You got it!!";
+        var feedback = "*Nioce! You got it!!*";
         document.getElementById("questionOneFeedback").innerHTML = feedback
     }
 }
@@ -50,17 +50,17 @@ function Question2(cnum)
 {
     if (cnum == 1)
     {
-        var feedback = "Oh ho! A flatter!";
+        var feedback = "*Oh ho! A flatter!*";
         document.getElementById("questionTwoFeedback").innerHTML = feedback
     }
     if (cnum == 2)
     {
-        var feedback = "I've aged well for 25 havent I ;)";
+        var feedback = "*I've aged well for 25 havent I*";
         document.getElementById("questionTwoFeedback").innerHTML = feedback
     }
     if (cnum == 3)
     {
-        var feedback = "Back up.";
+        var feedback = "*Back up.*";
         document.getElementById("questionTwoFeedback").innerHTML = feedback
     }
 }
@@ -69,17 +69,17 @@ function Question3(dnum)
 {
     if (dnum == 1)
     {
-        var feedback = "Oh My Goodness! I like you.";
+        var feedback = "*Oh My Goodness! I like you.*";
         document.getElementById("questionThreeFeedback").innerHTML = feedback
     }
     if (dnum == 2)
     {
-        var feedback = "Wow! You really think so?";
+        var feedback = "*Wow! You really think so?*";
         document.getElementById("questionThreeFeedback").innerHTML = feedback
     }
     if (dnum == 3)
     {
-        var feedback = "Come now, don't be greedy.";
+        var feedback = "*Come now, don't be greedy.*";
         document.getElementById("questionThreeFeedback").innerHTML = feedback
     }
 }
@@ -88,5 +88,6 @@ function Question3(dnum)
 function Counter()
 {
     wowCount=wowCount +1;
+    alert("WOW!!");
     document.getElementById("showCount").innerHTML = wowCount;
 }
