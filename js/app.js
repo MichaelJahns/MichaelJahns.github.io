5635cc2c88203510af50b9c881bd9cc1ada98d94
Michael <Jahnsmic001@gmail.com>
2018-10-09 15:55:56 -0700

    Added second multiple choice question

--- js/app.js
+++ js/app.js
@@ -31,22 +31,39 @@ function Question1(bnum)
 {
     if (bnum == 1)
     {
-        var feedback = "Nice one dumby";
+        var feedback = "Nice one dumby.";
         document.getElementById("questionOneFeedback").innerHTML = feedback
     }
     if (bnum == 2)
     {
-        var feedback = "Almost";
+        var feedback = "Almost.";
         document.getElementById("questionOneFeedback").innerHTML = feedback
     }
     if (bnum == 3)
     {
-        var feedback = "Nioce!";
+        var feedback = "Nioce! You got it!!";
         document.getElementById("questionOneFeedback").innerHTML = feedback
     }
 }
 //Question two
-
+function Question2(cnum)
+{
+    if (cnum == 1)
+    {
+        var feedback = "Oh ho! A flatter!";
+        document.getElementById("questionTwoFeedback").innerHTML = feedback
+    }
+    if (cnum == 2)
+    {
+        var feedback = "I've aged well for 25 havent I ;)";
+        document.getElementById("questionTwoFeedback").innerHTML = feedback
+    }
+    if (cnum == 3)
+    {
+        var feedback = "Back up.";
+        document.getElementById("questionTwoFeedback").innerHTML = feedback
+    }
+}
 //Question three
 
 
