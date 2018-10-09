015f5d6bc35d4e7ea1d0eea9908a6ffcc7607e92
Michael <Jahnsmic001@gmail.com>
2018-10-09 14:43:53 -0700

    Adds Question 1

--- js/app.js
+++ js/app.js
@@ -4,12 +4,36 @@
 //Declare Variables
 var wowCount = 0;
 
-//Get Users Name and return to page
-var user = prompt('Yo whats playing player.');
+alert ('Greetings stranger. It seems you have stumbled upon my Secret Garden.');
+
+//Get Users Name and returns value to page
+var user = prompt(' Before we begin, what may I ask is your name?');
 document.getElementById("username").innerHTML = user;
+console.log("The user input their name as " + user + ".s");
 
 
+//Question One
+var gate = prompt(' Well, ' + user + '! Are you ready to be dazzled!?').toLowerCase();
 
+if (gate.toLowerCase() === "yes" || gate.toLowerCase() === "y")
+{
+alert("Well, " +user + " Let us begin!");
+console.log(user + " is ready.");
+}
+else
+{
+alert("TOO BAD!");
+console.log(user + " is not ready.");
+};
+
+//Question two
+
+//Question three
+
+//Question four
+
+
+//WOWZA counter
 function Counter()
 {
     wowCount=wowCount +1;
