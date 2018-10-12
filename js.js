abe9851bec5759301db2beaa4ac6cb479ceb2f8c
Michael <Jahnsmic001@gmail.com>
2018-10-12 09:16:30 -0700

    build for code review

---
+++ js.js
@@ -0,0 +1,17 @@
+'use strict';
+
+var q1 = ['Where is Michael J go to College?','wsu', 'washington state university', 'washington state']
+var q2 = [10]
+var q3 = ['maelstrom', 'doody', 'phantasmagoric', ' chaos', 'olive', 'buttress', 'fjord']
+var q4 = ['rainbow']
+var q5 = ['heck', 'pumpkin head', 'yikes']
+var q6 = ['just enough to be healthy but not enough to be excessive']
+var q7 = ['yes', 'y']
+
+function questionBank(q)
+{
+	var call = 'q' + q;
+	alert(call[1])
+	alert('')
+
+}
